d9253879b2c8f2d1b75fdc6aee77fb50aef93ed1
Ibrahim Abo Elhassan <ibrahimmohamed29719@gmail.com>
2023-10-23 22:48:05 +0300

    Labs of MCU interfaing

---
+++ Unit_8_MCU_Interfacing/L3_U8_USART_driver/CODE/Drivers/Driver/Debug/stm32_f103c6_driver/KeyPad_Driver/keypad.d
@@ -0,0 +1,11 @@
+stm32_f103c6_driver/KeyPad_Driver/keypad.o: \
+ ../stm32_f103c6_driver/KeyPad_Driver/keypad.c \
+ ../stm32_f103c6_driver/KeyPad_Driver/keypad.h \
+ D:/Embedded\ System\ dip_KS/Unit_7_Projects/Drivers/Driver/stm32_f103c6_driver/inc/stm32f103x6.h \
+ D:/Embedded\ System\ dip_KS/Unit_7_Projects/Drivers/Driver/stm32_f103c6_driver/inc/GPIO_Driver_stm32f103c6.h
+
+../stm32_f103c6_driver/KeyPad_Driver/keypad.h:
+
+D:/Embedded\ System\ dip_KS/Unit_7_Projects/Drivers/Driver/stm32_f103c6_driver/inc/stm32f103x6.h:
+
+D:/Embedded\ System\ dip_KS/Unit_7_Projects/Drivers/Driver/stm32_f103c6_driver/inc/GPIO_Driver_stm32f103c6.h:
---
+++ Unit_8_MCU_Interfacing/L3_U8_USART_driver/CODE/Drivers/Driver/Debug/stm32_f103c6_driver/subdir.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../stm32_f103c6_driver/GPIO_Driver_stm32f103c6.c \
+../stm32_f103c6_driver/RCC_Driver_stm32f103c6.c \
+../stm32_f103c6_driver/USART_Driver_stm32f103c6.c 
+
+OBJS += \
+./stm32_f103c6_driver/GPIO_Driver_stm32f103c6.o \
+./stm32_f103c6_driver/RCC_Driver_stm32f103c6.o \
+./stm32_f103c6_driver/USART_Driver_stm32f103c6.o 
+
+C_DEPS += \
+./stm32_f103c6_driver/GPIO_Driver_stm32f103c6.d \
+./stm32_f103c6_driver/RCC_Driver_stm32f103c6.d \
+./stm32_f103c6_driver/USART_Driver_stm32f103c6.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+stm32_f103c6_driver/GPIO_Driver_stm32f103c6.o: ../stm32_f103c6_driver/GPIO_Driver_stm32f103c6.c
+	arm-none-eabi-gcc -gdwarf-2 "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C6Tx -DDEBUG -c -I../Inc -I"D:/Embedded System dip_KS/Unit_7_Projects/Drivers/Driver/stm32_f103c6_driver/KeyPad_Driver" -I"D:/Embedded System dip_KS/Unit_7_Projects/Drivers/Driver/stm32_f103c6_driver/LCD_Driver" -I"D:/Embedded System dip_KS/Unit_7_Projects/Drivers/Driver/stm32_f103c6_driver/inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"stm32_f103c6_driver/GPIO_Driver_stm32f103c6.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+stm32_f103c6_driver/RCC_Driver_stm32f103c6.o: ../stm32_f103c6_driver/RCC_Driver_stm32f103c6.c
+	arm-none-eabi-gcc -gdwarf-2 "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C6Tx -DDEBUG -c -I../Inc -I"D:/Embedded System dip_KS/Unit_7_Projects/Drivers/Driver/stm32_f103c6_driver/KeyPad_Driver" -I"D:/Embedded System dip_KS/Unit_7_Projects/Drivers/Driver/stm32_f103c6_driver/LCD_Driver" -I"D:/Embedded System dip_KS/Unit_7_Projects/Drivers/Driver/stm32_f103c6_driver/inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"stm32_f103c6_driver/RCC_Driver_stm32f103c6.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+stm32_f103c6_driver/USART_Driver_stm32f103c6.o: ../stm32_f103c6_driver/USART_Driver_stm32f103c6.c
+	arm-none-eabi-gcc -gdwarf-2 "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C6Tx -DDEBUG -c -I../Inc -I"D:/Embedded System dip_KS/Unit_7_Projects/Drivers/Driver/stm32_f103c6_driver/KeyPad_Driver" -I"D:/Embedded System dip_KS/Unit_7_Projects/Drivers/Driver/stm32_f103c6_driver/LCD_Driver" -I"D:/Embedded System dip_KS/Unit_7_Projects/Drivers/Driver/stm32_f103c6_driver/inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"stm32_f103c6_driver/USART_Driver_stm32f103c6.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
